623df8ff9f606f0d31b891700d88f3761f99dc2e
Paola2005 <velascopaola2005@gmail.com>
2023-11-07 13:11:58 -0500

    feat: :tada: Continue

---
+++ Persistence/Data/Configuration/CustomerConfiguration.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configuration
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(e => e.Id).HasName("PRIMARY");
+
+            builder.ToTable("customer");
+
+            builder.HasIndex(e => e.IdTipoPersonaFk, "IX_customer_IdTipoPersonaFk");
+
+            builder.HasIndex(e => e.IdcityFk, "IX_customer_IdcityFk");
+
+            builder.Property(e => e.DateRegister).HasColumnName("date_register");
+            builder.Property(e => e.Idcustomer).HasMaxLength(255);
+            builder.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+
+            builder.HasOne(d => d.IdTipoPersonaFkNavigation).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.IdTipoPersonaFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasOne(d => d.IdcityFkNavigation).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.IdcityFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+}
---
+++ Persistence/Data/PaolitaContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Data;
+
+public partial class PaolitaContext : DbContext
+{
+    public PaolitaContext()
+    {
+    }
+
+    public PaolitaContext(DbContextOptions<PaolitaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<PersonType> PersonTypes { get; set; }
+
+    public virtual DbSet<State> States { get; set; }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+}
